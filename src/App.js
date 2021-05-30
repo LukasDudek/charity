dbe8031c7e147ff11ca706b5febb80ff8d05af18
Lukas <dudeklukasz10@gmail.com>
2021-05-31 00:26:37 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
